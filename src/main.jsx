d2b74400990dcd786708b4e357598cd76063283a
Reza9002 <rezadawari@yahoo.com>
2026-06-07 12:42:18 +0200

    add  vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import TeamFinder from './components/TeamFinder.jsx';
-
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 	
 		<TeamFinder />
-	
+	<Analytics />
 	</React.StrictMode>
 );
